5462b73e69875562a14c7794e26743ee060839e2
Brian Bender <brian.bender@timesys.com>
2018-12-13 14:06:00 -0500

    Add support for custom pty suffix

--- teeterm.c
+++ teeterm.c
@@ -10,27 +10,34 @@
 #include <unistd.h>
 #include <pty.h>
 #include <signal.h>
+#include <string.h>
 #include <errno.h>
 #include <sys/fcntl.h>
+#include <limits.h>
 
 uint8_t buf[BUFSIZ];
 /* Ensure ts starts initialized to zero */
 struct termios ts;
+char ptyNames[2][PATH_MAX];
 
 void cleanup(int signo)
 {
-	if(unlink("pty0") < 0) {
-		perror("Could not unlink pty0");
-		if(unlink("pty1") < 0) {
-			perror("Could not unlink pty1");
+	if(unlink(ptyNames[0]) < 0) {
+		perror("Could not unlink pty*0");
+		if(unlink(ptyNames[1]) < 0) {
+			perror("Could not unlink pty*1");
 		}
 		exit(1);
 	}
-	if(unlink("pty1") < 0) {
-		perror("Could not unlink pty1");
+	if(unlink(ptyNames[1]) < 0) {
+		perror("Could not unlink pty*1");
 		exit(1);
 	}
 	exit(0);
+}
+
+void usage(char *self) {
+	printf("Usage: %s [-n pty_suffix] [ARGS]\n", self);
 }
 
 int main(int argc, char* argv[])
@@ -38,6 +45,9 @@ int main(int argc, char* argv[])
 	int child_pty;
 	int master[2];
 	int slave[2];
+
+	//char ptyNames[2][PATH_MAX];
+	int customname;
 
 	fd_set readfds;
 	int nfds;
@@ -47,8 +57,23 @@ int main(int argc, char* argv[])
 	pid_t pid;
 
 	if(argc < 2) {
-		printf("Usage: teeterm COMMAND [ARGS]\n");
+		usage(argv[0]);
 		return 1;
+	}
+
+	customname = !strcmp("-n", argv[1]);
+	if (customname && argc < 4) {
+		usage(argv[0]);
+		return 1;
+	}
+	
+	for (int i = 0; i <= 1; ++i) {
+		strcpy(ptyNames[i], "pty");
+		if (customname) {
+			strcat(ptyNames[i], argv[2]);
+		}
+		strcat(ptyNames[i], (i==0 ? "0" : "1"));
+		printf("customname: %d, ptyNames[%d]: %s\n", customname, i, ptyNames[i]);
 	}
 
 	cfmakeraw(&ts);
@@ -60,7 +85,9 @@ int main(int argc, char* argv[])
 		perror("Could not fork");
 		return 1;
 	} else if(pid == 0) {
-		execvp(argv[1], &(argv[1]));
+		int index = customname ? 3 : 1;
+		printf("Command: %s", argv[index]);
+		execvp(argv[index], &(argv[index]));
 		return 1;
 	} else {
 		nfds = child_pty;
@@ -74,7 +101,7 @@ int main(int argc, char* argv[])
 			}
 			fcntl(master[i], F_SETFL, O_NONBLOCK);
 			puts(ttyname(slave[i]));
-			if(symlink(ttyname(slave[i]), (i == 0) ? "pty0" : "pty1") < 0) {
+			if(symlink(ttyname(slave[i]), ptyNames[i]) < 0) {
 				perror("Could not set pty link");
 				return 1;
 			}
